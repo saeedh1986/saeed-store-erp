8f431436f1fdac0b6aa9e152bceed8930b4a6acf
Saeed Alhebsi <saeed@s3eed.ae>
2025-12-12 14:26:07 +0400

    Fix: Redirect root URL to admin panel

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # FUTURE: API URLs will be included here
     path('api/v1/inventory/', include('inventory.urls')),
     path('api/v1/customers/', include('contacts.urls')),
+    # Redirect root to admin
+    path('', RedirectView.as_view(url='/admin/', permanent=False)),
 ]
